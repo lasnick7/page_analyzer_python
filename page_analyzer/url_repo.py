1c67b4509e4a840f6a68258005b7f4203edbb8ce
lasnick7 <lasnick711@gmail.com>
2025-08-25 17:40:26 +0300

    new connection

--- page_analyzer/url_repo.py
+++ page_analyzer/url_repo.py
@@ -23,7 +23,7 @@ DATABASE_URL = os.getenv('DATABASE_URL')
 
 class UrlRepo:
     def __init__(self):
-        self.conn = psycopg2.connect(DATABASE_URL)
+        self.db = DATABASE_URL
 
     @staticmethod
     def validate(url):
@@ -41,61 +41,65 @@ class UrlRepo:
         return f"{parsed.scheme}://{parsed.netloc}"
 
     def save(self, name):
-        with self.conn.cursor() as curs:
-            sql = """
-            INSERT INTO urls
-            (name) VALUES (%s)
-            RETURNING id;
-            """
-            curs.execute(sql, (name,))
-            id = curs.fetchone()[0]
-        self.conn.commit()
-        return id
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor() as curs:
+                sql = """
+                INSERT INTO urls
+                (name) VALUES (%s)
+                RETURNING id;
+                """
+                curs.execute(sql, (name,))
+                id = curs.fetchone()[0]
+            conn.commit()
+            return id
 
     def find_url_by_name(self, name):
-        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            sql = """
-            SELECT * FROM urls
-            WHERE name = %s;
-            """
-            curs.execute(sql, (name,))
-            row = curs.fetchone()
-            return UrlItem(
-                name=row.name,
-                id=row.id,
-                created_at=row.created_at.isoformat()
-            ) if row else None
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                sql = """
+                SELECT * FROM urls
+                WHERE name = %s;
+                """
+                curs.execute(sql, (name,))
+                row = curs.fetchone()
+                return UrlItem(
+                    name=row.name,
+                    id=row.id,
+                    created_at=row.created_at.isoformat()
+                ) if row else None
 
     def find_url_by_id(self, id):
-        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            sql = """
-            SELECT * FROM urls
-            WHERE id = %s;
-            """
-            curs.execute(sql, (id,))
-            row = curs.fetchone()
-            return UrlItem(
-                name=row.name,
-                id=row.id,
-                created_at=row.created_at.isoformat()
-            ) if row else None
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                sql = """
+                SELECT * FROM urls
+                WHERE id = %s;
+                """
+                curs.execute(sql, (id,))
+                row = curs.fetchone()
+                return UrlItem(
+                    name=row.name,
+                    id=row.id,
+                    created_at=row.created_at.isoformat()
+                ) if row else None
 
     def get_content(self):
-        with self.conn.cursor(cursor_factory=NamedTupleCursor) as curs:
-            try:
-                sql = """
-                SELECT id, name FROM urls
-                ORDER BY id DESC;
-                """
-                curs.execute(sql)
-                res = []
-                for item in curs.fetchall():
-                    res.append(UrlItem(
-                        id=item.id,
-                        name=item.name
-                    ))
-                return res
-            except Exception as e:
-                print(f"Error executing SQL: {e}")
-                self.conn.rollback()
-                raise
+        with psycopg2.connect(DATABASE_URL) as conn:
+            with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
+                try:
+                    sql = """
+                    SELECT id, name FROM urls
+                    ORDER BY id DESC;
+                    """
+                    curs.execute(sql)
+                    res = []
+                    for item in curs.fetchall():
+                        res.append(UrlItem(
+                            id=item.id,
+                            name=item.name
+                        ))
+                    return res
+                except Exception as e:
+                    print(f"Error executing SQL: {e}")
+                    conn.rollback()
+                    raise
